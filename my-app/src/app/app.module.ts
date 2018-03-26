b91fb78b7a564b56ddf82d0d4ea3969bac0058b9
snelakanti <vnelakanti@gmail.com>
2018-03-26 15:57:53 -0400

    create recipe-detail component inside recipes component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
