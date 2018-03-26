5f443576de5d7c958e333f95c3bc567e9d9adafc
snelakanti <vnelakanti@gmail.com>
2018-03-26 15:53:16 -0400

    create recipes component with cli (previously header manual)

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
